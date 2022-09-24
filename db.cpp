9f45bec7b3d2bca81df23a4ed72784bf212efa30
Zeonx9 <mushtukov.artem@mail.ru>
2022-09-24 23:39:45 +0300

    use aliases, fix the bad_alloc() problem with generate(), use []operator

--- db.cpp
+++ db.cpp
@@ -3,9 +3,10 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include "db.h"
 
-int DataBase::find(const std::string &name) {
+int DataBase::find(const str &name) {
     for (int i = 0; i < names.size(); ++i) {
         if (names[i].first == name) {
             return i;
@@ -14,82 +15,45 @@ int DataBase::find(const std::string &name) {
     return -1;
 }
 
-void DataBase::create(const std::string &rootFolder) {
+void DataBase::create(const str &rootFolder) {
     root = rootFolder;
 //    std::filesystem::create_directories(root);
 }
 
-// creates a table and binds it to db object
-void DataBase::generate(const std::string &name, DataBase::Type type, const std::string &src) {
-    if (find(name) != -1) {
-        std::cout << "such table already exists\n";
+void DataBase::open(const str &rootFolder) {
+    root = rootFolder;
+
+    ifst file(root + db_saved_data);
+    if (!file){
+        std::cout << "DB not found\n";
         return;
     }
+    int n;
+    file >> n;
 
-    // bind table to db
-    auto path = root + name + ".txt";
-    names.emplace_back(name, type);
+    for (int i = 0; i < n; ++i) {
+        str s; int t;
+        file >> s >> t;
+        // bind table to
+        names.emplace_back(s, (Type) t);
 
-    // create table and put it in the right place
-    // saves created table to file
-    switch (type) {
-        case Type::student :
-            if (!src.empty() and studs.size() == 0)
-                studs.createFrom(src);
-            studs.save(path);
-            break;
-        case Type::variant :
-            if (!src.empty() and vars.size() == 0)
-                vars.createFrom(src);
-            vars.save(path);
-            break;
-        case Type::distribution :
-            if (studs.size() > 0 and vars.size() > 0)
-                distribute(dstr, studs, vars);
-            else
-                std::cout << "No accesible student or variant information to make distribution\n";
-            dstr.save(path);
-    }
-}
-
-void DataBase::print(const std::string &name, std::ostream &os, bool toRead) {
-    auto ind = find(name);
-    if (ind == -1) {
-        std::cout << "No such table\n";
-        return;
-    }
-
-    // check for printing distribution to read it
-    if (names[ind].second == Type::distribution and toRead) {
-        for (auto row = dstr.getTable().begin() + 1; row < dstr.getTable().end(); ++row) {
-            Student s = studs.getTable()[row->id];
-            os << s.surname << " " << s.name << " " << s.patronymic << "\t" << vars.getTable()[row->var].path << "\n";
-        }
-        return;
-    }
-
-    // print a table
-    switch (names[ind].second) {
-        case Type::student : {
-            for (auto &t : studs.getTable())
-                os << t << "\n";
-            break;
-        }
-        case Type::variant : {
-            for (auto &t : vars.getTable())
-                os << t << "\n";
-            break;
-        }
-        case Type::distribution : {
-            for (auto &t : dstr.getTable())
-                os << t << "\n";
+        auto path = root + s + ".txt";
+        switch ((Type) t) {
+            case Type::student:
+                studs.open(path);
+                break;
+            case Type::variant:
+                vars.open(path);
+                break;
+            case Type::distribution:
+                dstr.open(path);
         }
     }
+    file.close();
 }
 
 void DataBase::close() {
-    std::ofstream file;
-    file.open(root + db_saved_data);
+    ofst file(root + db_saved_data);
 
     // write number of tables
     file << names.size() << "\n";
@@ -113,38 +77,74 @@ void DataBase::close() {
     file.close();
 }
 
-void DataBase::open(const std::string &rootFolder) {
-    root = rootFolder;
+// creates a table and binds it to db object
+void DataBase::generate(const str &name, DataBase::Type type, const str &src) {
+    for (auto &p: names) {
+        if (p.first == name or p.second == type) {
+            std::cerr << "\"" << name << "\" table already exists\n";
+            return;
+        }
+    }
 
-    std::ifstream file;
-    file.open(root + db_saved_data);
-    if (!file){
-        std::cout << "DB not found\n";
+    // bind table to db
+    auto path = root + name + ".txt";
+    names.emplace_back(name, type);
+
+    // create table and put it in the right place
+    // saves created table to file
+    switch (type) {
+        case Type::student :
+            if (!src.empty() and studs.size() == 0)
+                studs.createFrom(src);
+            studs.save(path);
+            break;
+        case Type::variant :
+            if (!src.empty() and vars.size() == 0)
+                vars.createFrom(src);
+            vars.save(path);
+            break;
+        case Type::distribution :
+            if (studs.size() > 0 and vars.size() > 0) {
+                distribute(dstr, studs, vars);
+            } else {
+                std::cerr << "No accessible student or variant information to make distribution\n";
+            }
+            dstr.save(path);
+    }
+}
+
+void DataBase::print(const str &name, ost &os, bool toRead)  {
+    auto ind = find(name);
+    if (ind == -1) {
+        std::cerr << "No such table: \"" << name << "\"\n";
         return;
     }
 
-    int n;
-    file >> n;
+    // check for printing distribution to read it
+    if (names[ind].second == Type::distribution and toRead) {
+        for (auto &row : dstr.getTable()) {
+            auto &s = studs[row.id];
+            os << s.surname << " " << s.name << " " << s.patronymic << "\t\t" << vars[row.var].path << "\n";
+        }
+        return;
+    }
 
-    for (int i = 0; i < n; ++i) {
-        std::string s;
-        int t;
-        file >> s >> t;
-        // bind table to
-        names.emplace_back(s, (Type) t);
-
-        auto path = root + s + ".txt";
-        switch ((Type) t) {
-            case Type::student:
-                studs.open(path);
-                break;
-            case Type::variant:
-                vars.open(path);
-                break;
-            case Type::distribution:
-                dstr.open(path);
+    // print a table
+    switch (names[ind].second) {
+        case Type::student : {
+            for (auto &t : studs.getTable())
+                os << t << "\n";
+            break;
+        }
+        case Type::variant : {
+            for (auto &t : vars.getTable())
+                os << t << "\n";
+            break;
+        }
+        case Type::distribution : {
+            for (auto &t : dstr.getTable())
+                os << t << "\n";
         }
     }
-    file.close();
 }
 
--- db.h
+++ db.h
@@ -12,22 +12,28 @@ class DataBase {
 public:
     enum class Type{student, variant, distribution};
 private:
-    const std::string db_saved_data = "this_file_contain_information_about_db_state.txt";
-    std::string root = "../data/";
-    std::vector<std::pair<std::string, Type>> names;
+    const str db_saved_data = "this_file_contain_information_about_db_state.txt";
+    str root = "../data/";
+    std::vector<std::pair<str, Type>> names;
 
     Table<Student> studs;
     Table<Variant> vars;
     Table<Distribution> dstr;
 
-    int find(const std::string &name);
+    int find(const str &name);
 
 public:
-    void create(const std::string &rootFolder);
-    void open(const std::string &rootFolder);
+    void create(const str &rootFolder);
+    void open(const str &rootFolder);
     void close();
-    void generate(const std::string &name, Type type, const std::string &src="");
-    void print(const std::string &name, std::ostream &os, bool toRead=false);
+
+    void generate(const str &name, Type type, const str &src="");
+    void print(const str &name, ost &os, bool toRead=false);
+
+    void add(const str &table, const str &line);
+    void remove(const str &table, int key);
+    void update(const str &table, int key, const str &line);
+    void printLine(const str &table, int key);
 };
 
 #endif //DATABASELABSE_DB_H
